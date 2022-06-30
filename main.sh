fb095fc269e987f38ca040714920cebb8f03bc51
Arka Roy Chowdhury <arkaroyc14@gmail.com>
2022-07-01 04:34:16 +0530

    ADD MAIN SCRIPT TO GET ALL TOP COIN ID NAME

---
+++ main.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#INSTALL JQ FIRST
+sudo apt install jq
+mkdir text
+mkdir json
+curl "https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active%2Cinactive%2Cuntracked&limit=10000&start=1" --output 'json/all.json'
+jq -r .data.cryptoCurrencyMap[].id 'json/all.json' > 'text/id.txt'
+jq -r .data.cryptoCurrencyMap[].symbol 'json/all.json' > 'text/symbol.txt'
+jq -r .data.cryptoCurrencyMap[].name 'json/all.json' > 'text/name.txt'
